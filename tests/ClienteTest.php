acb121fcfdc4d7a9364f3fa0d524851361fb7b34
IgorDePaula <principe.borodin@gmail.com>
2013-06-27 15:31:19 -0300

    insercao de tests

---
+++ tests/ClienteTest.php
@@ -0,0 +1,75 @@
+<?php
+
+use Doctrine\ORM\EntityManager,
+    Doctrine\ORM\Configuration;
+use API\Entity\Categorias;
+use \PHPUnit_Framework_TestCase;
+
+class ClienteTest extends PHPUnit_Framework_TestCase
+{
+    private $_object;
+    
+    public function setUp()
+    {
+        $applicationMode = "development";
+        if ($applicationMode == "development") {
+            $cache = new \Doctrine\Common\Cache\ArrayCache;
+        } else {
+            $cache = new \Doctrine\Common\Cache\ApcCache;
+        }
+
+        $config = new Configuration;
+        $config->setMetadataCacheImpl($cache);
+        $driverImpl = $config->newDefaultAnnotationDriver(
+            '../module/API/src/API/Entity'
+        );
+        $config->setMetadataDriverImpl($driverImpl);
+        $config->setQueryCacheImpl($cache);
+        $config->setProxyDir('/path/to/myproject/lib/MyProject/Proxies');
+        $config->setProxyNamespace('MyProject\Proxies');
+
+        if ($applicationMode == "development") {
+            $config->setAutoGenerateProxyClasses(true);
+        } else {
+            $config->setAutoGenerateProxyClasses(false);
+        }
+
+        $connectionOptions = array(
+            'driver' => 'pdo_mysql',
+            'host' => 'localhost',
+            'port' => '3306',
+            'user' => 'root',
+            'password' => '',
+            'dbname' => 'myapphome'
+        );
+
+        $entityManager = EntityManager::create($connectionOptions, $config);
+        $this->_object = new Categorias($entityManager);
+    }
+    
+    public function testSetGetCategoryName()
+    {
+        $this->_object->setCategoria('Massas');
+        $this->assertEquals('Massas', $this->_object->getCategoria());
+    }
+    public function testSetGetCategoryId()
+    {
+        $this->_object->setIdcategorias(1);
+        $this->assertEquals(1, $this->_object->getIdcategorias());
+    }
+    public function testSetGetGetTypeCategoryName()
+    {
+        $this->_object->setCategoria('Massas');
+        $this->assertEquals('string', gettype($this->_object->getCategoria()));
+    }
+    public function testSetGetGetTypeCategoryId()
+    {
+        $this->_object->setIdcategorias(1);
+        $this->assertEquals(
+            'integer', 
+            gettype($this->_object->getIdcategorias())
+        );
+    }
+     
+
+}
